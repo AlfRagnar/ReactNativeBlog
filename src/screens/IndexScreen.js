12b8b18031961bd39b07489f782d0abe5c119042
Alf-Ragnar Wirkola Masternes <alf-ragnar.wirkola@edunord.se>
2021-01-09 17:02:05 +0100

    Chapter 12 upto Episode 24

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,79 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import {
+    View,
+    Text,
+    FlatList,
+    Button,
+    StyleSheet,
+    TouchableOpacity,
+} from "react-native";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
     return (
         <View>
-            <Text>Index Screen</Text>
+            <Button title="Add post" onPress={addBlogPost} />
+            <FlatList
+                data={state}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity
+                            onPress={() =>
+                                navigation.navigate("Show", { id: item.id })
+                            }
+                        >
+                            <View style={styles.row}>
+                                <Text style={styles.title}>
+                                    {item.title} - {item.id}
+                                </Text>
+                                <TouchableOpacity
+                                    onPress={() => deleteBlogPost(item.id)}
+                                >
+                                    <Feather name="trash" style={styles.icon} />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        // eslint-disable-next-line react/display-name
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                <Feather name="plus" style={styles.iconHeader} />
+            </TouchableOpacity>
+        ),
+    };
+};
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor: "gray",
+        paddingHorizontal: 10,
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24,
+        color: "black",
+    },
+    iconHeader: {
+        fontSize: 24,
+        color: "black",
+    },
+});
 
 export default IndexScreen;
